c595ce54f112a8d14837602de90993b0772cb18e
Afwan <rizki.afwan99@gmail.com>
2024-10-06 22:02:29 +0700

    soal1

---
+++ src/main/java/com/juaracoding/rizkiujianjf/ujian1/soal1JavaVariabel.java
@@ -0,0 +1,17 @@
+package com.juaracoding.rizkiujianjf.ujian1;
+
+public class soal1JavaVariabel {
+    public static void main(String[] args) {
+
+        String namaFilm = "Avengers: Endgame";
+        int hargaTiket = 50000;
+        byte jumlahTiket = 3;
+
+        System.out.println("Nama Film \t:" +namaFilm);
+        System.out.println("Harga Tiket\t:" +hargaTiket);
+        System.out.println("Jumlah Tiket :" +jumlahTiket);
+
+    }
+}
+
+
